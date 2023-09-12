622410a818e20f53ef0eab03e9b34bf11317ae74
Brilliant-Developer1 <email2obail@gmail.com>
2023-09-12 22:34:58 +0600

    first commit

---
+++ prims_mst.cpp
@@ -0,0 +1,119 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// #include<bits/stdc++.h>
+#define pi pair<int, int>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<pi> v[N];
+bool visit[N];
+
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+class cmp
+{
+public:
+    bool operator()(Edge a, Edge b)
+    {
+        return a.w > b.w;
+    }
+};
+
+void prims(int s)
+{
+    priority_queue<Edge, vector<Edge>, cmp> pq;
+    pq.push(Edge(1, 2, 10));
+    pq.push(Edge(1, 5, 5));
+
+    while (!pq.empty())
+    {
+        Edge p = pq.top();
+        pq.pop();
+        cout << p.w << endl;
+    }
+}
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        v[a].push_back({b, w});
+        v[b].push_back({a, w});
+    }
+
+    prims(1);
+
+    return 0;
+}

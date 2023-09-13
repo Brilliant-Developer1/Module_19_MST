618eb9a1618dbba799a1d7d7b46848a15838632f
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-13 21:36:44 +0600

    prims mst added

--- Kruskal_s.cpp
+++ Kruskal_s.cpp
@@ -78,12 +78,58 @@ bool cmp(Edge a, Edge b)
     return a.w < b.w;
 }
 
+const int N = 1e5 + 5;
+int parent[N];
+int parentSize[N];
+
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parentSize[i] = 1;
+    }
+}
+
+int dsu_find(int node)
+{
+
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+
+void dsu_union(int a, int b)
+{
+    int leaderA = dsu_find(a);
+    int leaderB = dsu_find(b);
+
+    if (leaderA != leaderB)
+    {
+        if (parentSize[leaderA] > parentSize[leaderB])
+        {
+            // A leader
+            parent[leaderB] = leaderA;
+            parentSize[leaderA] += parentSize[leaderB];
+        }
+        else
+        {
+            parent[leaderA] = leaderB;
+            parentSize[leaderB] += parentSize[leaderA];
+        }
+    }
+}
+
 int main()
 {
 
     int n, e;
     cin >> n >> e;
     vector<Edge> v;
+    vector<Edge> ans;
+    dsu_set(n);
     while (e--)
     {
         int a, b, w;
@@ -94,6 +140,18 @@ int main()
     sort(v.begin(), v.end(), cmp);
 
     for (Edge val : v)
+    {
+        int a = val.a;
+        int b = val.b;
+        int w = val.w;
+        int leaderA = dsu_find(a);
+        int leaderB = dsu_find(b);
+        if (leaderA == leaderB)
+            continue;
+        ans.push_back(val);
+        dsu_union(a, b);
+    }
+    for (Edge val : ans)
     {
         cout << val.a << " " << val.b << " " << val.w << endl;
     }
@@ -116,5 +174,10 @@ int main()
 6 7 9
 
 
-
+1 2 5
+5 6 5
+2 4 6
+1 3 7
+3 6 7
+6 7 9
 */
